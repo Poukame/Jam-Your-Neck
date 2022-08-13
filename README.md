3f393e29830aef3dc91bd974ec0e6b1bb564065b
Poukame <theretg@gmail.com>
2022-08-13 19:53:24 +0300

    feat: :sparkles: Added warning message for screen below 600px

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Jam-Your-Neck
 An app to learn the guitar scales while having fun jamming on track
+
+## Why this app?
+Hi, I'm Guillaume. I created this app because while playing on backing tracks from Youtube, I was tired of searching on Google for an image of the neck with the scale of the track I was playing over.
+
+This app allows you to get the notes on the neck in a couple of clicks. It helped me save time and play more. I hope it can help other fellow guitarists as well. Have fun playing scales!
+
+## Limitations
+If you use the app on your smartphone, use it in landscape mode. The guitar neck can't be wrapped, I've done my best to make it useable down to 600px width, which should accommodate most nowadays phones.
+
+When searching for a song, you might not always get the result you want. This is due to the API I use. Unfortunately, I can't sort the songs by popularity. If you want to find the key of a song, please use the link below to GetSongKey.
+
+## Credits
+This app is fetching the songs' data from ![GetSongKey.com](https://getsongkey.com/) API . Thanks to them.
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
-import { Text, Box, Flex, HStack, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import {
+	Text,
+	Box,
+	Flex,
+	HStack,
+	Tabs,
+	TabList,
+	TabPanels,
+	Tab,
+	TabPanel,
+	Alert,
+	AlertIcon,
+	AlertTitle,
+	AlertDescription,
+	keyframes
+} from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import AppHeader from './Components/Header';
 import RootNoteOption from './Components/RootNoteOption';
 import ToneOption from './Components/ToneOption';
@@ -10,8 +26,22 @@ import RootMarkerOption from './Components/RootMarkerOption';
 import { GoSettings, GoSearch, GoInfo } from 'react-icons/go';
 import SongSearch from './Components/SongSearch';
 import About from './Components/About';
+import useWindowDimensions from './assets/WindowSizeHook';
+
+
+const animationKeyframes = keyframes`
+  0% {  }
+  50% {  }
+  100% { opacity: 1 }
+  25% {  }
+  75% { opacity: 0 }
+`;
+
+const animation = `${animationKeyframes} 2s infinite both;`;
 
 function App() {
+	const { width } = useWindowDimensions();
+
 	return (
 		<Box p='4' minW='600px'>
 			<AppHeader />
@@ -19,22 +49,34 @@ function App() {
 				<TabList gap='4'>
 					<Tab color='black' _selected={{ color: 'white', bg: 'orange.300' }} bg='orange.100'>
 						<GoSettings />
-						<Text ml='10px' letterSpacing='wide'>Options</Text>
+						<Text ml='10px' letterSpacing='wide'>
+							Options
+						</Text>
 					</Tab>
 					<Tab color='black' _selected={{ color: 'white', bg: 'orange.300' }} bg='orange.100'>
 						<GoSearch />
-						<Text ml='10px' letterSpacing='wide'>Find Song Key</Text>
+						<Text ml='10px' letterSpacing='wide'>
+							Find Song Key
+						</Text>
 					</Tab>
 					<Tab color='black' _selected={{ color: 'white', bg: 'orange.300' }} bg='orange.100'>
 						<GoInfo />
-						<Text ml='10px' letterSpacing='wide'>About</Text>
+						<Text ml='10px' letterSpacing='wide'>
+							About
+						</Text>
 					</Tab>
 				</TabList>
 
 				<TabPanels mb='8'>
 					<TabPanel>
 						<RootNoteOption />
-						<Flex gap='8' rowGap={{base: '2', md: '4', lg:'8'}} mb='8' alignItems='flex-end' flexWrap='wrap' >
+						<Flex
+							gap='8'
+							rowGap={{ base: '2', md: '4', lg: '8' }}
+							mb='8'
+							alignItems='flex-end'
+							flexWrap='wrap'
+						>
 							<ScaleTypeOption />
 							<ToneOption />
 							<NotationOption />
@@ -53,7 +95,15 @@ function App() {
 				</TabPanels>
 			</Tabs>
 			<Flex direction='column' gap='6'>
-				<Neck />
+				{width > 600 ? (
+					<Neck />
+				) : (
+					<Alert status='error' >
+						<AlertIcon as={motion.div} animation={animation}/>
+						<AlertTitle>The screen is a bit small!</AlertTitle>
+						<AlertDescription>Please use your phone in landscape to view the guitar neck</AlertDescription>
+					</Alert>
+				)}
 			</Flex>
 		</Box>
 	);
