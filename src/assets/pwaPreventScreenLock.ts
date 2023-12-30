bcf2a3616c1affde4d09812023c147a1203674fd
Poukame <theretg@gmail.com>
2023-12-30 18:03:17 +0100

    fix for navigator TS

--- src/assets/pwaPreventScreenLock.ts
+++ src/assets/pwaPreventScreenLock.ts
@@ -1,24 +1,30 @@
-	// code to make WPA prevents screen dimming and locking
-	// The wake lock sentinel.
-	let wakeLock:any = null;
+const navigatorWithWakeLock = navigator as Navigator & { wakeLock: any };
 
-	// Function that attempts to request a screen wake lock.
-	const requestWakeLock = async () => {
-		try {
-			wakeLock = await navigator.wakeLock.request();
-			wakeLock.addEventListener('release', () => {
-				console.log('Screen Wake Lock released:', wakeLock.released);
-			});
-			console.log('Screen Wake Lock released:', wakeLock.released);
-		} catch (err:any) {
-			console.error(`${err.name}, ${err.message}`);
-		}
-	};
 
-    const handleVisibilityChange = async () => {
-		if (wakeLock !== null && document.visibilityState === 'visible') {
-			await requestWakeLock();
-		}
-	};
+// The wake lock sentinel.
+let wakeLock: any = null;
 
-export {requestWakeLock, handleVisibilityChange}
+// Function that attempts to request a screen wake lock.
+const requestWakeLock = async () => {
+  try {
+    if ('wakeLock' in navigatorWithWakeLock) {
+      wakeLock = await navigatorWithWakeLock.wakeLock.request();
+      wakeLock.addEventListener('release', () => {
+        console.log('Screen Wake Lock released:', wakeLock.released);
+      });
+      console.log('Screen Wake Lock released:', wakeLock.released);
+    } else {
+      console.error('Wake Lock API not supported');
+    }
+  } catch (err: any) {
+    console.error(`${err.name}, ${err.message}`);
+  }
+};
+
+const handleVisibilityChange = async () => {
+  if (wakeLock !== null && document.visibilityState === 'visible') {
+    await requestWakeLock();
+  }
+};
+
+export { requestWakeLock, handleVisibilityChange };
